bbc77f1755c9893ca572578a5acc4887a1a59ada
Gokhan Kici <gokhankici@gmail.com>
2013-12-24 21:16:41 +0200

    ready for tests

--- pin/Bloom.h
+++ pin/Bloom.h
@@ -10,7 +10,7 @@
 #define DEFAULT_BLOOM_FILTER_SIZE 2048
 #define ADDR_SIZE 8
 #define BLOOM_ADDR(address) ((const unsigned char*) &address)
-#define SET_OVERRIDE
+//#define SET_OVERRIDE
 
 typedef unsigned int (*hashfunc_t)(const unsigned char *);
 
